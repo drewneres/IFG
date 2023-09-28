a873f61ff222ab93c4cf605150e794c57db29d26
Hendrew Neres <71440544+drewneres@users.noreply.github.com>
2023-09-28 19:42:30 -0300

    Create soma.py

---
+++ soma.py
@@ -0,0 +1,10 @@
+def soma3(lol, cs, cod):
+    soma = lol + cs + cod
+    return soma
+
+print("Soma de três valores")
+lol = int(input("Primeiro número: "))
+cs = int(input("Segundo número: "))
+cod = int(input("Terceiro número: "))
+
+print("Soma = ", soma3(lol, cs, cod))
