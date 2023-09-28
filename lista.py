209cec7a30279d216cd4f4c1f603cded9f711277
Hendrew Neres <71440544+drewneres@users.noreply.github.com>
2023-09-28 19:54:04 -0300

    Create lista.py

---
+++ lista.py
@@ -0,0 +1,141 @@
+arquivo = open("exemplo01.txt", "w+")
+for i in range(1):
+    arquivo.write("Bacteria, Penicillin, Streptomycin, Neomycin, Gram %d\n" % (i+1))
+arquivo.close()
+
+arquivo = open("exemplo01.txt", "r")
+for linha in arquivo.readlines():
+    print (linha)
+arquivo = open("exemplo01.txt", "w+")
+for i in range(1):
+    arquivo.write("Mycobacterium tuberculosis,800,5,2,negative %d\n" % (i+1))
+arquivo.close()
+
+arquivo = open("exemplo01.txt", "r")
+for linha in arquivo.readlines():
+    print (linha)
+arquivo = open("exemplo01.txt", "w+")
+for i in range(1):
+    arquivo.write("Salmonella schottmuelleri,10,0.8,0.09,negative %d\n" % (i+1))
+arquivo.close()
+
+arquivo = open("exemplo01.txt", "r")
+for linha in arquivo.readlines():
+    print (linha)
+
+arquivo = open("exemplo01.txt", "w+")
+for i in range(1):
+    arquivo.write("Proteus vulgaris,3,0.1,0.1,negative %d\n" % (i+1))
+arquivo.close()
+
+arquivo = open("exemplo01.txt", "r")
+for linha in arquivo.readlines():
+    print (linha)
+
+arquivo = open("exemplo01.txt", "w+")
+for i in range(1):
+    arquivo.write("Klebsiella pneumoniae,850,1.2,1,negative %d\n" % (i+1))
+arquivo.close()
+
+arquivo = open("exemplo01.txt", "r")
+for linha in arquivo.readlines():
+    print (linha)
+
+arquivo = open("exemplo01.txt", "w+")
+for i in range(1):
+    arquivo.write("Brucella abortus,1,2,0.02,negative %d\n" % (i+1))
+arquivo.close()
+
+arquivo = open("exemplo01.txt", "r")
+for linha in arquivo.readlines():
+    print (linha)
+
+arquivo = open("exemplo01.txt", "w+")
+for i in range(1):
+    arquivo.write("Pseudomonas aeruginosa,850,2,0.4,negative %d\n" % (i+1))
+arquivo.close()
+
+arquivo = open("exemplo01.txt", "r")
+for linha in arquivo.readlines():
+    print (linha)
+
+arquivo = open("exemplo01.txt", "w+")
+for i in range(1):
+    arquivo.write("Escherichia coli,100,0.4,0.1,negative %d\n" % (i+1))
+arquivo.close()
+
+arquivo = open("exemplo01.txt", "r")
+for linha in arquivo.readlines():
+    print (linha)
+
+arquivo = open("exemplo01.txt", "w+")
+for i in range(1):
+    arquivo.write("Salmonella (Eberthella) typhosa,1,0.4,0.008,negative %d\n" % (i+1))
+arquivo.close()
+
+arquivo = open("exemplo01.txt", "r")
+for linha in arquivo.readlines():
+    print (linha)
+
+arquivo = open("exemplo01.txt", "w+")
+for i in range(1):
+    arquivo.write("Aerobacter aerogenes,870,1,1.6,negative %d\n" % (i+1))
+arquivo.close()
+
+arquivo = open("exemplo01.txt", "r")
+for linha in arquivo.readlines():
+    print (linha)
+
+arquivo = open("exemplo01.txt", "w+")
+for i in range(1):
+    arquivo.write("Brucella antracis,0.001,0.01,0.007,positive %d\n" % (i+1))
+arquivo.close()
+
+arquivo = open("exemplo01.txt", "r")
+for linha in arquivo.readlines():
+    print (linha)
+
+arquivo = open("exemplo01.txt", "w+")
+for i in range(1):
+    arquivo.write("Streptococcus fecalis,1,1,0.1,positive %d\n" % (i+1))
+arquivo.close()
+
+arquivo = open("exemplo01.txt", "r")
+for linha in arquivo.readlines():
+    print (linha)
+
+arquivo = open("exemplo01.txt", "w+")
+for i in range(1):
+    arquivo.write("Staphylococcus aureus,0.03,0.03,0.001,positive %d\n" % (i+1))
+arquivo.close()
+
+arquivo = open("exemplo01.txt", "r")
+for linha in arquivo.readlines():
+    print (linha)
+
+arquivo = open("exemplo01.txt", "w+")
+for i in range(1):
+    arquivo.write("Streptococcus hemolyticus,0.001,14,10,positive %d\n" % (i+1))
+arquivo.close()
+
+arquivo = open("exemplo01.txt", "r")
+for linha in arquivo.readlines():
+    print (linha)
+
+arquivo = open("exemplo01.txt", "w+")
+for i in range(1):
+    arquivo.write("Streptococcus viridans,0.005,10,40,positive %d\n" % (i+1))
+arquivo.close()
+
+arquivo = open("exemplo01.txt", "r")
+for linha in arquivo.readlines():
+    print (linha)
+
+arquivo = open("exemplo01.txt", "w+")
+for i in range(1):
+    arquivo.write("Diplococcus pneumoniae,0.005,11,10,positive %d\n" % (i+1))
+arquivo.close()
+
+arquivo = open("exemplo01.txt", "r")
+for linha in arquivo.readlines():
+    print (linha)
