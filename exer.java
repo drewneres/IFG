11abb84b3099ad331353026219dcc04b215e99cb
Hendrew Neres <71440544+drewneres@users.noreply.github.com>
2023-09-28 20:33:57 -0300

    Create exer.java

---
+++ exer.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	  for (int i= 1; i<= 50; i++){
+	      if(i % 2!=- 0){
+	          System.out.println(i);
+	      }
+	    }
+	  }
+	}
