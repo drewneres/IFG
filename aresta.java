2759a610853406b1352fd4768f771fbbc44e8347
Hendrew Neres <71440544+drewneres@users.noreply.github.com>
2023-09-30 13:27:12 -0300

    Create aresta.java

---
+++ aresta.java
@@ -0,0 +1,43 @@
+public class Aresta{
+    
+    int peso;
+    Vertice inicio;
+    Vertice fim;
+
+    public Aresta(int peso, Vertice inicio, Vertice fim) {
+        this.peso = peso;
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public Vertice getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Vertice inicio) {
+        this.inicio = inicio;
+    }
+
+    public Vertice getFim() {
+        return fim;
+    }
+
+    public void setFim(Vertice fim) {
+        this.fim = fim;
+    }
+
+ 
+    
+    
+    
+    
+    
+}
